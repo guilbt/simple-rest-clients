21d3b4384cb106830b63f92da4fc580933968710
Guilherme Batista <guilherme.batista@armateus.com.br>
2019-10-16 14:08:15 -0300

    D - SimpleRestClient - Create simple abstraction to springs' RestTemplate

---
+++ src/main/java/com/guilbt/simplerestclients/SimpleRestClient.java
@@ -0,0 +1,100 @@
+package com.guilbt.simplerestclients;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+public class SimpleRestClient {
+    private final String server;
+    private final RestTemplate rest;
+    private final HttpHeaders headers;
+
+    public SimpleRestClient(String server, Map<String, String> addHeaders) {
+        Assert.notNull(server, "Server parameter can't be null");
+        this.server = server;
+        this.rest = new RestTemplate();
+        this.headers = new HttpHeaders();
+        this.headers.add("Content-Type", "application/json");
+        this.headers.add("Accept", "application/json");
+        if(addHeaders!=null) {
+            for (Map.Entry<String, String> header : addHeaders.entrySet()) {
+                this.headers.add(header.getKey(), header.getValue());
+            }
+        }
+    }
+
+    public SimpleRestClient(Map<String, String> addHeaders) {
+        this("", addHeaders);
+    }
+
+    public SimpleRestClient() {
+        this("", null);
+    }
+
+    public SimpleRestClient(String server) {
+        this(server, null);
+    }
+
+    public <E> ResponseEntity<E> get(String uri, Class<E> returnObjectClass, HttpHeaders addHeaders) {
+        addHeaders.addAll(headers);
+        HttpEntity<String> requestEntity = new HttpEntity<>("", addHeaders);
+        return rest.exchange(server + uri, HttpMethod.GET, requestEntity, returnObjectClass);
+    }
+
+    public <E, T> ResponseEntity<E> post(String endpoint, T json, Class<E> returnObjectClass, HttpHeaders addHeaders) {
+        addHeaders.addAll(headers);
+        HttpEntity<T> requestEntity = new HttpEntity<>(json, addHeaders);
+        String URL = server + endpoint;
+        return rest.exchange(URL, HttpMethod.POST, requestEntity, returnObjectClass);
+    }
+
+    public <E, T> ResponseEntity<E> put(String uri, T json, Class<E> returnObjectClass, HttpHeaders addHeaders) {
+        addHeaders.addAll(headers);
+        HttpEntity<T> requestEntity = new HttpEntity<>(json, addHeaders);
+        return rest.exchange(server + uri, HttpMethod.PUT, requestEntity, returnObjectClass);
+    }
+
+    public <E> ResponseEntity<E> delete(String uri, Class<E> returnObjectClass, HttpHeaders addHeaders) {
+        addHeaders.addAll(headers);
+        HttpEntity<String> requestEntity = new HttpEntity<>("", addHeaders);
+        return rest.exchange(server + uri, HttpMethod.DELETE, requestEntity, returnObjectClass);
+    }
+
+
+    public <E> ResponseEntity<E> get(String uri, Class<E> returnObjectClass) {
+        return this.get(uri, returnObjectClass, new HttpHeaders());
+    }
+
+    public <E, T> ResponseEntity<E> post(String endpoint, T json, Class<E> returnObjectClass) {
+        return this.post(endpoint, json, returnObjectClass, new HttpHeaders());
+    }
+
+    public <E, T> ResponseEntity<E> put(String uri, T json, Class<E> returnObjectClass) {
+        return this.put(uri, json, returnObjectClass, new HttpHeaders());
+    }
+
+    public <E> ResponseEntity<E> delete(String uri, Class<E> returnObjectClass) {
+        return this.delete(uri, returnObjectClass, new HttpHeaders());
+    }
+
+    public ResponseEntity<String> get(String uri) {
+        return this.get(uri, String.class);
+    }
+
+    public <T> ResponseEntity<String> post(String uri, T json) {
+        return this.post(uri, json, String.class);
+    }
+
+    public <T> ResponseEntity<String> put(String uri, T json) {
+        return this.put(uri, json, String.class);
+    }
+
+    public ResponseEntity<String> delete(String uri) {
+        return this.delete(uri, String.class);
+    }
+}
---
+++ src/test/java/com/guilbt/simplerestclients/SimpleRestClientTest.java
@@ -0,0 +1,65 @@
+package com.guilbt.simplerestclients;
+
+import org.springframework.web.client.RestClientException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SimpleRestClientTest {
+    private SimpleRestClient fakeClient;
+    private final String fakeEndpoint = "/batata";
+
+    @Test
+    public void testInstantiation() {
+        SimpleRestClient drc = new SimpleRestClient("anyUrl", new HashMap<>());
+        Assert.assertNotNull(drc);
+    }
+
+    @Test
+    public void testInstantiationWithAdditionalHeaders() {
+        Map<String, String> addHeaders = new HashMap<String, String>();
+        addHeaders.put("testing", "testing");
+        SimpleRestClient drc = new SimpleRestClient(addHeaders);
+        Assert.assertNotNull(drc);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInstantiationWithNullServer() {
+        String nullTestString = null;
+        new SimpleRestClient(nullTestString);
+    }
+
+    @Test
+    public void testInstantiationWithoutParams() {
+        new SimpleRestClient();
+    }
+
+    @Test(expected = RestClientException.class)
+    public void testGetResourceAccessException() {
+        fakeClient.get(fakeEndpoint);
+    }
+
+    @Test(expected = RestClientException.class)
+    public void testPostResourceAccessException() {
+        fakeClient.post(fakeEndpoint, "");
+    }
+
+    @Test(expected = RestClientException.class)
+    public void testPutResourceAccessException() {
+        fakeClient.put(fakeEndpoint, "");
+    }
+
+    @Test(expected = RestClientException.class)
+    public void testDeleteResourceAccessException() {
+        fakeClient.delete(fakeEndpoint);
+    }
+
+    @Before
+    public void setup() {
+        fakeClient = new SimpleRestClient("http://fake_url.doesntexist", new HashMap<>());
+    }
+}
